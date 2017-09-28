a55ed39e61bb34249e3e25186d07438e1d93595c
Marigold17 <30533071+Marigold17@users.noreply.github.com>
2017-09-28 15:05:00 -0700

    Create myjscript.js

---
+++ myjscript.js
@@ -0,0 +1,9 @@
+var username;
+var message;
+username = 'Molly';
+message ='See our upcoming events';
+
+var elName = document.getElementById('name'};
+elName.textContent = username;
+var elNote =document.getElementById('note');
+elNote.textContent = message;
