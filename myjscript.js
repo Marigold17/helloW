79bcb20da54f83c436cb4f69b3b1ce0d2000ce22
Marigold17 <30533071+Marigold17@users.noreply.github.com>
2017-09-30 15:28:24 -0700

    Update myjscript.js

--- myjscript.js
+++ myjscript.js
@@ -3,7 +3,7 @@ var hourNow = today.getHours();
 var greeting;
 
 if (hourNow > 18){
-    greeting = 'Good evening';
+    greeting = 'Good Evening!';
 }else if (hourNow > 12){
     greeting = 'Good Afternoon!';
 }else if(hourNow > 0){
