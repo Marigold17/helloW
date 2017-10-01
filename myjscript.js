de9dc4c9f769dc1b57f016193933065637a787b3
Marigold17 <30533071+Marigold17@users.noreply.github.com>
2017-10-01 16:41:02 -0700

    Update myjscript.js

--- myjscript.js
+++ myjscript.js
@@ -1,22 +1,7 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow > 18){
-    greeting = 'Good Evening!';
-}else if (hourNow > 12){
-    greeting = 'Good Afternoon!';
-}else if(hourNow > 0){
-    greeting = 'Good morning!';
-}else {
-  greeting = 'Welcome!';
-}
-document.write(greeting);
-
-var firstName = 'Julie';
-var lastName = 'Molly';
+var greeting = 'Julie';
+var name = 'Molly';
 var message = ' Please check your order: ';
-var welcome = firstName  + lastName + message;
+var welcome = greeting  + name + message;
 
 var sign = 'Montage House';
 var tiles = sign.length;
@@ -24,11 +9,14 @@ var subTotal = tiles *5;
 var shipping = 7;
 var grandTotal= subTotal + shipping;
 
-var el = document.getElementById('firstName');
+var el = document.getElementById('greeting');
 el.textContent = welcome;
 
 var elSign = document.getElementById('userSign');
 elSign.textContent = sign;
+
+var elShipping =document.getElementById('shipping');
+elShipping.textContent='$' + shipping;
 
 
 
