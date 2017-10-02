fa5f6707adca7ae8203c0f93295808da4d661c91
Marigold17 <30533071+Marigold17@users.noreply.github.com>
2017-10-02 13:12:33 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,22 @@
 function myFunction() {
    document.getElementById("demo2").innerHTML = "Paragraph has been changed.";
 }
+var greeting = 'Julie';
+var name = 'Molly';
+var message = ' Please check your order: ';
+var welcome = greeting  + name + message;
+
+var sign = 'Montage House';
+var tiles = sign.length;
+var subTotal = tiles *5;
+var shipping = 7;
+var grandTotal= subTotal + shipping;
+
+var el = document.getElementById('greeting');
+el.textContent = welcome;
+
+var elSign = document.getElementById('userSign');
+elSign.textContent = sign;
+
+var elShipping =document.getElementById('shipping');
+elShipping.textContent='$' + shipping;
