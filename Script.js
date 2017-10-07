a48901436512de7c06404585aea7b34a6e575e28
Marigold17 <30533071+Marigold17@users.noreply.github.com>
2017-10-06 17:02:43 -0700

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,12 @@
 document.write(5 + 6);
 
+
 function myFunction() {
    document.getElementById("demo2").innerHTML = "Paragraph has been changed.";
 }
 
+
+var a = 100;
+var b = 50;
+var x = a + b;
+document.getElementById("demox").innerHTML = x;
